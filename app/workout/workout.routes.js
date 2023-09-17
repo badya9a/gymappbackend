9e0dd4897d6f706cba9484592f1c536683c86d73
Kozikov Bohdan <kozikovbohdan@Kozikovs-MacBook-Air.local>
2023-09-18 02:45:13 +0300

    first commit

---
+++ app/workout/workout.routes.js
@@ -0,0 +1,36 @@
+import express from 'express'
+
+import { protect } from '../middleware/auth.middleware.js'
+
+import { getWorkoutLog } from './log/get-workout-log.controller.js'
+import { updateCompleteWorkoutLog } from './log/update-workout-log.controller.js'
+import { createNewWorkoutLog } from './log/workout-log.controller.js'
+import {
+	createNewWorkout,
+	deleteWorkout,
+	getWorkout,
+	getWorkouts,
+	getWorkoutsByUser,
+	updateWorkout
+} from './workout.controller.js'
+
+const router = express.Router()
+
+router.route('/').post(protect, createNewWorkout).get(protect, getWorkouts)
+
+router.route('/user/:id').get(protect, getWorkoutsByUser)
+
+router
+	.route('/:id')
+	.get(protect, getWorkout)
+	.put(protect, updateWorkout)
+	.delete(protect, deleteWorkout)
+
+router
+	.route('/log/:id')
+	.post(protect, createNewWorkoutLog)
+	.get(protect, getWorkoutLog)
+
+router.route('/log/complete/:id').patch(protect, updateCompleteWorkoutLog)
+
+export default router
